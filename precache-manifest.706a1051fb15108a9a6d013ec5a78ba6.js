0dd6e5ccf3d5be395c5b99b1f82b9f54d2400688
FadyAttia <fadyattia11@gmail.com>
2020-09-09 14:23:16 +0200

    Updates

---
+++ precache-manifest.706a1051fb15108a9a6d013ec5a78ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8980086649be30d45936cfc8ac73b58d",
+    "url": "/4for-v2/index.html"
+  },
+  {
+    "revision": "36c17decdb492280c6b2",
+    "url": "/4for-v2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99b175c53094de80437e",
+    "url": "/4for-v2/static/js/2.14470cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/4for-v2/static/js/2.14470cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c17decdb492280c6b2",
+    "url": "/4for-v2/static/js/main.c813df7f.chunk.js"
+  },
+  {
+    "revision": "89dbd0d5d71d1e91058c",
+    "url": "/4for-v2/static/js/runtime-main.14d22d0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/4for-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
